f040018082715d4416798903ea71fa4fdc97af5a
清田徳敬 <Nori@Noribook.local>
2021-03-07 11:33:39 +0900

    drinktask

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,15 @@
-class BonusDrink
-  def self.total_count_for(amount)
-    0
-  end
-end
+p "set the first amount"
+amount=gets().to_i
+a01= 0
+bonus= 0
+while amount>=3
+   a01+=amount-amount%3
+   #printf("a01:%d\n",a01)
+   bonus= (amount-amount%3)/3
+   #printf("bonus:%d\n",bonus)
+   amount= bonus+amount%3
+   #printf("amount:%d\n",amount)
+end  
+a01+=amount
+p "total"
+p a01
