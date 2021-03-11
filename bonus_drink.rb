945e38109b5a462a0ac2767ec9707ec6cac7f90e
清田徳敬 <Nori@Noribook.local>
2021-03-11 10:57:36 +0900

    p is deleted

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,14 +1,13 @@
 class BonusDrink
   def self.total_count_for(amount)
-    a01= 0
+    total= 0
     bonus= 0
     while amount>=3
-       a01+=amount-amount%3
-       bonus= (amount-amount%3)/3
-       amount= bonus+amount%3
-     end  
-    a01+=amount
-    p a01
-    return a01
+      total+= amount-amount%3
+      bonus= (amount-amount%3)/3
+      amount= bonus+amount%3
+    end  
+    total+= amount
+    return total
   end
 end
